25ea84ef3cda4f4660182b3043a5ca7fecc92bcb
Mathias Lindgren <mathias.lindgren@stabelo.se>
2022-04-02 17:16:28 +0200

    Getter of data.

--- sparse_set.h
+++ sparse_set.h
@@ -8,14 +8,13 @@ struct SparseSetData {
 	D data;
 };
 
-
 size_t INVALID_INDEX = -1;
 template<typename D>
 struct SparseSet {
 	std::vector<SparseSetData<D>> dense;
 	std::vector<size_t> sparse;
 
-	void assign(size_t index, D d) {
+	D* assign(size_t index, D d) {
 		if(sparse.size() <= index) {
 			sparse.resize((index+1) * 1.2, INVALID_INDEX);
 		}
@@ -25,6 +24,8 @@ struct SparseSet {
 			sparse[index] = dense.size();
 			dense.push_back(SparseSetData<D>{index, d});
 		}
+
+		return get(index);
 	}
 
 	void remove(size_t index) {
@@ -38,6 +39,13 @@ struct SparseSet {
 
 	bool lookup(size_t index) {
 		return sparse[index] < dense.size() && dense[sparse[index]].index == index;
+	}
+
+	D* get(size_t index) {
+		if(lookup(index)) {
+			return &dense[sparse[index]].data;
+		}
+		return nullptr;
 	}
 };
 
